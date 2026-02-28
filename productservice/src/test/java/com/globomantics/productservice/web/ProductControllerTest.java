fdc75603b7be2a8a92d1f551c4ed236d96224228
Bill Ho <ycho.bill.lab@gmail.com>
2026-03-01 00:06:41 +0800

    feat: Testing the Controller - Add unit tests (Part I)

---
+++ productservice/src/test/java/com/globomantics/productservice/web/ProductControllerTest.java
@@ -0,0 +1,60 @@
+package com.globomantics.productservice.web;
+
+import com.globomantics.productservice.model.Product;
+import com.globomantics.productservice.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @MockBean
+    private ProductService service;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("GET /product/1 - Found")
+    public void testGetProductByIdFound() throws Exception {
+        // Setup our mocked service
+        Product mockProduct = new Product(1, "Product Name", 10, 1);
+        doReturn(Optional.of(mockProduct)).when(service).findById(1);
+
+        // Execute the GET request
+        mockMvc.perform(get("/product/1"))
+
+                // Validate the response code and content type
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+                // Validate the headers
+                .andExpect(header().string(HttpHeaders.ETAG, "\"1\""))
+                .andExpect(header().string(HttpHeaders.LOCATION, "/product/1"))
+
+                // Validate the returned fields
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Product Name")))
+                .andExpect(jsonPath("$.quantity", is(10)))
+                .andExpect(jsonPath("$.version", is(1)));
+    }
+}
