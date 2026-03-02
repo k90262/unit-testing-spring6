abbe01b4320c5eb770f5be1739a41f0ba4ee0200
Bill Ho <ycho.bill.lab@gmail.com>
2026-03-02 23:40:10 +0800

    feat: Testing the Controller - Add unit tests (Part II) - GET /product/1 - Not Found, POST /product - Success

--- productservice/src/test/java/com/globomantics/productservice/web/ProductControllerTest.java
+++ productservice/src/test/java/com/globomantics/productservice/web/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package com.globomantics.productservice.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.globomantics.productservice.model.Product;
 import com.globomantics.productservice.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
@@ -17,9 +18,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
@@ -56,5 +59,51 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$.name", is("Product Name")))
                 .andExpect(jsonPath("$.quantity", is(10)))
                 .andExpect(jsonPath("$.version", is(1)));
+    }
+
+    @Test
+    @DisplayName("GET /product/1 - Not Found")
+    public void testGetProductByIdNotFound() throws Exception {
+        // Setup our mocked service
+        doReturn(Optional.empty()).when(service).findById(1);
+
+        // Execute the GET request
+        mockMvc.perform(get("/product/1"))
+
+                // Validate that we get a 404 Not Found response
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("POST /product - Success")
+    public void testCreateProduct() throws Exception {
+        // Setup our mocked service
+        Product postProduct = new Product(1, "Product Name", 10);
+        Product mockProduct = new Product(1, "Product Name", 10, 1);
+        doReturn(mockProduct).when(service).save(any());
+
+        mockMvc.perform(post("/product").contentType(MediaType.APPLICATION_JSON).content(asJsonString(postProduct)))
+
+                // Validate the response code and content type
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+                // Validate the headers
+                .andExpect(header().string(HttpHeaders.ETAG, "\"1\""))
+                .andExpect(header().string(HttpHeaders.LOCATION, "/product/1"))
+
+                // Validate the returned fields
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Product Name")))
+                .andExpect(jsonPath("$.quantity", is(10)))
+                .andExpect(jsonPath("$.version", is(1)));
+    }
+
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
